d8e453cd822cf2a2b087c86e3998a93ad867dc36
Michael Fossel <Michael Fossel>
2016-03-12 08:46:15 -0800

    added accounts pages

---
+++ PizzaMan.Web/js/account/LoginController.js
@@ -0,0 +1,3 @@
+﻿angular.module('app').controller('LoginController', function ($scope) {
+
+});
--- PizzaMan.Web/js/app.js
+++ PizzaMan.Web/js/app.js
@@ -9,6 +9,8 @@ angular.module('app').config(function ($stateProvider, $urlRouterProvider, $http
         .state('home', { url: '/home', templateUrl: '/templates/home/home.html', controller: 'HomeController' })
           .state('home.dashboard', { url: '/dashboard', parent:'home', templateUrl: '/templates/dashboard/dashboard.html', controller: 'DashboardController' })
           .state('home.review', { url: '/review', parent: 'home', templateUrl: '/templates/review/review.html', controller: 'ReviewController' })
+          .state('home.login', { url: '/login', parent: 'home', templateUrl: '/templates/account/login.html', controller: 'LoginController' })
+          .state('home.register', { url: '/register', parent: 'home', templateUrl: '/templates/account/register.html', controller: 'RegisterController' })
     ;
 });
 
