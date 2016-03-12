866f2bdd74ea6f5385c99bc22ad533f05f58a95d
Michael Fossel <Michael Fossel>
2016-03-11 22:57:59 -0800

    fixed routing and added to front end

--- PizzaMan.Web/js/app.js
+++ PizzaMan.Web/js/app.js
@@ -7,8 +7,8 @@ angular.module('app').config(function ($stateProvider, $urlRouterProvider, $http
     $urlRouterProvider.otherwise('home');
     $stateProvider
         .state('home', { url: '/home', templateUrl: '/templates/home/home.html', controller: 'HomeController' })
-          .state('home.dashboard', { url: '/dashboard', templateUrl: '/templates/dashboard/dashboard.html', controller: 'DashboardController' })
-    
+          .state('home.dashboard', { url: '/dashboard', parent:'home', templateUrl: '/templates/dashboard/dashboard.html', controller: 'DashboardController' })
+          .state('home.review', { url: '/review', parent: 'home', templateUrl: '/templates/review/review.html', controller: 'ReviewController' })
     ;
 });
 
--- PizzaMan.Web/js/home/home.ctlr.js
+++ PizzaMan.Web/js/home/home.ctlr.js
@@ -1,4 +1,4 @@
-﻿angular.module('app').controller('HomeController', function ($scope, AuthenticationService) {
-    $scope.loginData = {};
+﻿angular.module('app').controller('HomeController', function ($scope) {
+   
 
 });
