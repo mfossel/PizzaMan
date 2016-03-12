59bb3f61415e12dfcd0f0de4b23f40c83ab9349b
Michael Fossel <Michael Fossel>
2016-03-11 19:06:28 -0800

    fixing ui router

--- PizzaMan.Web/js/app.js
+++ PizzaMan.Web/js/app.js
@@ -1,12 +1,15 @@
 ﻿angular.module('app', ['ngResource', 'ui.router', 'LocalStorageModule']);
 
-angular.module('app').value('apiUrl', 'http://localhost:49834/api');
+angular.module('app').value('apiUrl', 'http://localhost:63608/api/');
 
 angular.module('app').config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     $httpProvider.interceptors.push('AuthenticationInterceptor');
-    $urlRouterProvider.otherwise('app');
+    $urlRouterProvider.otherwise('home');
     $stateProvider
-        .state('app', { url: '/app', templateUrl: '/templates/app/app.html', controller: 'AppController' });
+        .state('home', { url: '/home', templateUrl: '/templates/home/home.html', controller: 'HomeController' })
+          .state('home.dashboard', { url: '/dashboard', templateUrl: '/templates/dashboard/dashboard.html', controller: 'DashboardController' })
+    
+    ;
 });
 
 angular.module('app').run(function (AuthenticationService) {
--- PizzaMan.Web/js/infrastructure/authentication.interceptor.js
+++ PizzaMan.Web/js/infrastructure/authentication.interceptor.js
@@ -1,1 +1,24 @@
-﻿
+﻿angular.module('app').factory('AuthenticationInterceptor', function ($q, localStorageService) {
+    function interceptRequest(request) {
+        var token = localStorageService.get('token');
+
+        if (token) {
+            request.headers.Authorization = 'Bearer ' + token.token;
+        }
+
+        return request;
+    }
+
+    function interceptResponse(response) {
+        if (response.status === 401) {
+            location.replace('/#/home'); //we will look here if we will get a problem using the login :))))))
+        }
+
+        return $q.reject(response);
+    }
+
+    return {
+        request: interceptRequest,
+        responseError: interceptResponse
+    };
+});
