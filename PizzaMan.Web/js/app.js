37d7b53d418ba9930dc06243753a04f748991e47
Mike Fossel <mikefossel@gmail.com>
2016-03-11 17:04:12 -0800

    working on ui routing

---
+++ PizzaMan.Web/js/app.js
@@ -0,0 +1,14 @@
+﻿angular.module('app', ['ngResource', 'ui.router', 'LocalStorageModule']);
+
+angular.module('app').value('apiUrl', 'http://localhost:49834/api');
+
+angular.module('app').config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+    $httpProvider.interceptors.push('AuthenticationInterceptor');
+    $urlRouterProvider.otherwise('app');
+    $stateProvider
+        .state('app', { url: '/app', templateUrl: '/templates/app/app.html', controller: 'AppController' });
+});
+
+angular.module('app').run(function (AuthenticationService) {
+    AuthenticationService.initialize();
+});
